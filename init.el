6d2d5f84f2941e1133df5447026554c3bbb3ca45
zhujunqi <junqi.zhu@coollu.com.cn>
2022-07-24 14:45:39 +0800

    Remap M-x(s-z) C-x(C-z)

--- init.el
+++ init.el
@@ -99,11 +99,11 @@
 ;; <----------------------------------------------------------------------------------------------------
 ;; global keys
 
-(global-unset-key (kbd "s-h"))          ; hide emacs
-(global-unset-key (kbd "s-z"))          ; undo
-(global-unset-key (kbd "C-x C-c"))      ; quit emacs
-(global-unset-key (kbd "C-z"))          ; minimize emacs
-;; (global-unset-key (kbd "C-x C-z"))      ; minimize emacs
+;; remap C-x to C-z
+(global-unset-key (kbd "C-x"))
+(global-set-key (kbd "C-z") ctl-x-map)
+
+(global-unset-key (kbd "C-z C-c"))      ; quit emacs
 
 (global-set-key [f3] 'toggle-truncate-lines)
 (global-set-key [f4] 'kmacro-end-or-call-macro)
@@ -224,6 +224,8 @@
 ;; C-x g	magit-status
 ;; C-x M-g	magit-dispatch
 ;; C-c M-g	magit-file-dispatch
+
+(global-set-key (kbd "C-z g") 'magit-status)
 ;; >----------------------------------------------------------------------------------------------------
 
 
@@ -300,11 +302,14 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-z b") 'helm-mini)
 
 ;; https://github.com/emacs-helm/helm/wiki#helm-mode
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; remap M-x to s-z
+(global-unset-key (kbd "M-x"))
+(global-set-key (kbd "s-z") 'helm-M-x)
+
+(global-set-key (kbd "C-z C-f") 'helm-find-files)
 
 ;; (global-set-key (kbd "C-c h o") 'helm-occur)
 (global-set-key [f8] 'helm-occur)
@@ -321,7 +326,7 @@
 (global-set-key [f11] 'helm-do-ag)
 
 ;; (require 'helm-ls-git)
-(global-set-key (kbd "C-x C-d") 'helm-browse-project)
+(global-set-key (kbd "C-z C-d") 'helm-browse-project)
 ;; >----------------------------------------------------------------------------------------------------
 
 
