dd32703fde8e45f06af662257c994c19e2603c52
zhujunqi <junqi.zhu@coollu.com.cn>
2022-07-26 11:48:20 +0800

    set company-dabbrev-downcase, https://github.com/company-mode/company-mode/issues/14#issuecomment-290261406

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(company-dabbrev-downcase nil)
  '(create-lockfiles nil)
  '(custom-safe-themes
    '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
