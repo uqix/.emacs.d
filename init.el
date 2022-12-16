7de653d64b0ac9216a0e6d704faf977da11d6b79
zhujunqi <junqi.zhu@coollu.com.cn>
2022-12-16 13:41:25 +0800

    Rename fun my-java-mode-hook to my/java-mode-hook

--- init.el
+++ init.el
@@ -265,10 +265,10 @@
 
 ;; https://www.gnu.org/software/emacs/manual/html_node/efaq/Customizing-C-and-C_002b_002b-indentation.html
 ;;
-(defun my-java-mode-hook ()
+(defun my/java-mode-hook ()
   (c-set-offset 'arglist-cont-nonempty '++)
   (c-set-offset 'arglist-intro '++))
-(add-hook 'java-mode-hook 'my-java-mode-hook)
+(add-hook 'java-mode-hook 'my/java-mode-hook)
 ;; >----------------------------------------------------------------------------------------------------
 
 
