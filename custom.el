045ff87d0b0fff78a34240a06843e5d3f4fb8f8e
zhujunqi <junqi.zhu@coollu.com.cn>
2022-12-18 09:14:17 +0800

    Enable company-show-quick-access for M-(digit)

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(company-dabbrev-downcase nil)
+ '(company-show-quick-access t)
  '(create-lockfiles nil)
  '(custom-safe-themes
    '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
