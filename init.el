4ca7e405badaf7a8917008ac7698bb5cf1f6d402
zhujunqi <junqi.zhu@coollu.com.cn>
2022-12-19 18:08:55 +0800

    Enable flyspell-mode for prog-mode

--- init.el
+++ init.el
@@ -63,6 +63,7 @@
 ;; flyspell
 
 (add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-mode)
 ;; >----------------------------------------------------------------------------------------------------
 
 
