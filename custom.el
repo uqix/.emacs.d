117f5d89effcc32069b2a10d15af61f9b796618c
zhujunqi <junqi.zhu@coollu.com.cn>
2022-12-20 09:27:09 +0800

    Set aw-leading-char-face and aw-background

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(aw-background nil)
  '(column-number-mode t)
  '(company-dabbrev-downcase nil)
  '(company-show-quick-access t)
@@ -73,6 +74,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco"))))
+ '(aw-leading-char-face ((t (:foreground "blue" :weight bold :height 2.0))))
  '(flyspell-duplicate ((t (:underline "gray85"))))
  '(flyspell-incorrect ((t (:underline "gray85"))))
  '(line-number ((t (:inherit (shadow default) :background "ivory" :foreground "gray92"))))
