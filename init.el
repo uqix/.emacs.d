85d824efa7e829102060bbfa5442b7f5a20fa4c1
zhujunqi <junqi.zhu@coollu.com.cn>
2023-01-12 10:13:08 +0800

    Bind key: er/expand-region

--- init.el
+++ init.el
@@ -225,7 +225,7 @@
 ;; <--------------------------------------------------
 ;; expand-region
 
-(global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "M-o") 'er/expand-region)
 ;; >--------------------------------------------------
 
 
