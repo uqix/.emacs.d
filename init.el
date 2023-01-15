706059e3308c6d23f7c3f85a8b81b044ac0c2155
uqix <zenkiezhu@gmail.com>
2023-01-15 18:09:08 +0800

    avy: add some doc

--- init.el
+++ init.el
@@ -381,6 +381,7 @@
 ;; https://github.com/abo-abo/avy#bindings
 ;;
 (avy-setup-default)
+;; C-' in isearch-mode-map -> avy-isearch
 
 (global-set-key (kbd "C-'") 'avy-goto-char-timer)
 (global-set-key (kbd "M-g g") 'avy-goto-line) ; was goto-line
