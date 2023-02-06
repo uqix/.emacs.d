e668780b178f2cbe396ba10c99b5db710e132eb8
zhujunqi <junqi.zhu@coollu.com.cn>
2023-02-06 15:25:20 +0800

    markdown-mode: bind keys for headings (next/prev sibling and up level)

--- init.el
+++ init.el
@@ -496,10 +496,13 @@
 
 (require 'markdown-mode)
 
-(define-key markdown-mode-map (kbd "C-c b") 'markdown-insert-gfm-code-block) ; code [b]lock
-(define-key markdown-mode-map (kbd "C-c q") 'markdown-insert-blockquote)     ; [q]uote
-(define-key markdown-mode-map (kbd "C-c e") 'markdown-insert-bold)           ; [e]mphasize
-(define-key markdown-mode-map (kbd "C-c l") 'markdown-insert-link)           ; [l]ink
+(define-key markdown-mode-map (kbd "C-c b") 'markdown-insert-gfm-code-block)       ; code [b]lock
+(define-key markdown-mode-map (kbd "C-c q") 'markdown-insert-blockquote)           ; [q]uote
+(define-key markdown-mode-map (kbd "C-c e") 'markdown-insert-bold)                 ; [e]mphasize
+(define-key markdown-mode-map (kbd "C-c l") 'markdown-insert-link)                 ; [l]ink
+(define-key markdown-mode-map (kbd "C-M-n") 'markdown-outline-next-same-level)     ; [n]ext sibling; was forward-list
+(define-key markdown-mode-map (kbd "C-M-p") 'markdown-outline-previous-same-level) ; [p]revious sibling; was backward-list
+(define-key markdown-mode-map (kbd "C-M-u") 'markdown-up-heading)                  ; [u]p level; was backward-up-list
 ;; >--------------------------------------------------
 
 
