09689333d8513205e71ee521567398659b4138f3
zhujunqi <junqi.zhu@coollu.com.cn>
2023-02-09 10:20:26 +0800

    Enable commands: narrow-to-region upcase-region downcase-region

--- init.el
+++ init.el
@@ -642,4 +642,9 @@
 ;; misc
 
 (electric-pair-mode)
+
+(put 'narrow-to-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
 ;; >--------------------------------------------------
