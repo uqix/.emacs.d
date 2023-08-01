6e8b4f3df5c03a186d492f90d760fe63d51f632e
zhujunqi <z.jq@winning.com.cn>
2023-08-01 10:30:52 +0800

    Set face for flyspell

--- custom.el
+++ custom.el
@@ -44,4 +44,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flyspell-duplicate ((t (:underline "gray85"))))
+ '(flyspell-incorrect ((t (:underline "gray85"))))
  '(markdown-code-face ((t (:extend t :background "gray97")))))
