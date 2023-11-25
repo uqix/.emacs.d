ae87283893871f138e60a7620dbe15d379fb2a2b
uqix <zenkiezhu@gmail.com>
2023-11-25 21:55:47 +0800

    Set face: eglot-highlight-symbol-face, markdown-header-face

--- custom.el
+++ custom.el
@@ -46,7 +46,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eglot-highlight-symbol-face ((t (:background "light goldenrod yellow"))))
+ '(eglot-highlight-symbol-face ((t (:background "light goldenrod yellow" :weight bold))))
  '(flyspell-duplicate ((t (:underline "gray85"))))
  '(flyspell-incorrect ((t (:underline "gray85"))))
- '(markdown-code-face ((t (:extend t :background "gray97")))))
+ '(markdown-code-face ((t (:extend t :background "gray97"))))
+ '(markdown-header-face ((t (:inherit bold :background "khaki1" :foreground "#e45649")))))
