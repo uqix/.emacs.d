310f6b8bdc143f7855d1712e689b57ccacc8307f
uqix <zenkiezhu@gmail.com>
2023-11-30 20:54:51 +0800

    Set cape-dabbrev-min-length, bind dabbrev-expand to default M-/

--- init.el
+++ init.el
@@ -33,20 +33,8 @@
 
 ;; * Use Dabbrev with Corfu!
 
-;; swap
-(global-set-key (kbd "M-/") 'dabbrev-completion)
-(global-set-key (kbd "C-M-/") 'dabbrev-expand)
+;; M-/ -> dabbrev-expand
 
-;; dabbrev-completion
-
-;; With a prefix argument ARG, it searches all buffers accepted by the
-;; function pointed out by ‘dabbrev-friend-buffer-function’ to find the
-;; completions.
-;;
-;; Its value is ‘dabbrev--same-major-mode-p’
-
-;; If the prefix argument is 16 (which comes from C-u C-u),
-;; then it searches *all* buffers.
 ;; >--------------------------------------------------
 
 
