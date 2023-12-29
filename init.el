bd4cce8a8c62ba894188c257f716a19d69d75b33
uqix <zenkiezhu@gmail.com>
2023-12-29 20:17:00 +0800

    Refactor

--- init.el
+++ init.el
@@ -893,10 +893,10 @@
 
 (global-set-key (kbd "C-c '") 'edit-indirect-region)
 
-(defun edit-indirect-guess-mode (_parent-buffer _beg _end)
+(defun my/edit-indirect/guess-mode (_parent-buffer _beg _end)
   (setq-local buffer-file-name (format "%s.-ei-" (buffer-file-name _parent-buffer)))
   (funcall (buffer-local-value 'major-mode _parent-buffer)))
-(setq edit-indirect-guess-mode-function #'edit-indirect-guess-mode)
+(setq edit-indirect-guess-mode-function #'my/edit-indirect/guess-mode)
 
 ;; <-------------------------
 ;; https://github.com/Fanael/edit-indirect/issues/6#issuecomment-387945773
@@ -906,27 +906,27 @@
 
 (defvar edit-indirect--left-margin 0)
 
-(defun vbe:compute-left-margin (code)
+(defun vbe/compute-left-margin (code)
   "Compute left margin of a string of code."
   (-min
    (-map #'(lambda (line) (length (car (s-match "^\\s-*" line))))
          (-remove 's-blank? (s-lines code)))))
 
-(defun vbe:edit-indirect-remove-left-margin ()
+(defun vbe/edit-indirect/remove-left-margin ()
   "Remove left-margin and save it into a local variable."
-  (let ((lm (vbe:compute-left-margin (buffer-substring (point-min) (point-max)))))
+  (let ((lm (vbe/compute-left-margin (buffer-substring (point-min) (point-max)))))
     (indent-rigidly (point-min) (point-max) (* -1 lm))
     (setq-local edit-indirect--left-margin lm)
     ;; https://github.com/Fanael/edit-indirect/issues/6#issuecomment-1055542145
     ;; buffer-local variable whose value should not be reset when changing major modes
     (put 'edit-indirect--left-margin 'permanent-local t)))
 
-(defun vbe:edit-indirect-restore-left-margin ()
+(defun vbe/edit-indirect/restore-left-margin ()
   "Restore left-margin before commiting."
   (indent-rigidly (point-min) (point-max) edit-indirect--left-margin))
 
-(add-hook 'edit-indirect-after-creation-hook #'vbe:edit-indirect-remove-left-margin)
-(add-hook 'edit-indirect-before-commit-hook #'vbe:edit-indirect-restore-left-margin)
+(add-hook 'edit-indirect-after-creation-hook #'vbe/edit-indirect/remove-left-margin)
+(add-hook 'edit-indirect-before-commit-hook #'vbe/edit-indirect/restore-left-margin)
 
 (require 'edit-indirect)
 ;; https://github.com/Fanael/edit-indirect/issues/6#issuecomment-1284144173
