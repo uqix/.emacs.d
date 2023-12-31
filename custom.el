155d83285774c694cc15daa7aae17d9c6e218c26
uqix <zenkiezhu@gmail.com>
2023-12-31 20:49:21 +0800

    Set face for corfu-current, same background as vertico-current

--- custom.el
+++ custom.el
@@ -48,6 +48,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(corfu-current ((t (:background "#42444a" :foreground "#bbc2cf"))))
  '(eglot-highlight-symbol-face ((t (:foreground "red" :weight bold))))
  '(flyspell-duplicate ((t (:underline nil))))
  '(font-lock-comment-face ((t (:foreground "dark cyan"))))
