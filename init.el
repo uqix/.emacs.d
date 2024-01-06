9f1cf85378f56d12f54793e0c72cfd0074d2bca8
uqix <zenkiezhu@gmail.com>
2024-01-06 10:35:26 +0800

    Bind key git-timemachine

--- init.el
+++ init.el
@@ -266,6 +266,8 @@
 
 (global-set-key (kbd "s-i r r") #'query-replace-regexp) ; [r]eplace: regexp
 
+(global-set-key (kbd "s-i v t") #'git-timemachine) ; [v]c: git-[t]imemachine
+
 (global-set-key (kbd "C-h F") #'describe-face) ; [h]elp: [F]ace
 
 ;; C-x r o -> open-rectangle
