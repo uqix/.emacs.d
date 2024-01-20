10eb038eb8cdc97685e89ac0bd5ea04014a5217f
uqix <zenkiezhu@gmail.com>
2024-01-20 10:30:07 +0800

    Delete package doom-modeline

--- custom.el
+++ custom.el
@@ -4,7 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(telephone-line gruvbox-theme eglot-booster pdf-tools all-the-icons dirvish eglot transient edit-indirect nginx-mode polymode groovy-mode git-timemachine jinja2-mode flymake-collection docker yaml-pro vundo doom-themes cape corfu wgrep vertico orderless embark-consult marginalia markdown-mode doom-modeline ztree multiple-cursors ace-window magit yasnippet symbol-overlay expand-region))
+   '(transient telephone-line gruvbox-theme eglot-booster pdf-tools all-the-icons dirvish eglot edit-indirect nginx-mode polymode groovy-mode git-timemachine jinja2-mode flymake-collection docker yaml-pro vundo doom-themes cape corfu wgrep vertico orderless embark-consult marginalia markdown-mode ztree multiple-cursors ace-window magit yasnippet symbol-overlay expand-region))
  '(package-vc-selected-packages
    '((eglot-booster :url "https://github.com/jdtsmith/eglot-booster.git" :branch "allow-initialization-options"))))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
