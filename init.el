5e7faf64d8ae4c788653f26044511b9708fc3ed9
zhujunqi <z.jq@winning.com.cn>
2024-05-21 14:59:13 +0800

    find-file, er-auto-create-missing-dirs

--- init.el
+++ init.el
@@ -2343,10 +2343,23 @@
 
 (keymap-global-set "s-i o" #'my/find-file/dev-notes)
 
+;; <-------------------------
+;; ## find-file
+
 (defun my/find-file/dev-notes ()
   (interactive)
   (let ((default-directory "~/Documents/notes/dev/"))
     (call-interactively #'find-file)))
+
+;; https://emacsredux.com/blog/2022/06/12/auto-create-missing-directories/
+(defun er-auto-create-missing-dirs ()
+  (let ((target-dir (file-name-directory buffer-file-name)))
+    (unless (file-exists-p target-dir)
+      (make-directory target-dir t))))
+
+(add-to-list 'find-file-not-found-functions #'er-auto-create-missing-dirs)
+;; >-------------------------
+
 ;; >--------------------------------------------------
 
 
