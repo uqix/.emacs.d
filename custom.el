6221bf562ac746576bd0e13b858e7b8f2f9a1ec8
zhujunqi <z.jq@winning.com.cn>
2024-10-11 10:27:49 +0800

    Install modus-themes

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(web-mode jq-mode highlight-function-calls dired-subtree just-mode justl difftastic diff-hl magit-todos powershell vterm doom-modeline flyspell-correct avy ace-window gruvbox-theme pdf-tools eglot edit-indirect nginx-mode polymode groovy-mode git-timemachine jinja2-mode flymake-collection yaml-pro vundo cape corfu wgrep vertico orderless embark-consult marginalia markdown-mode ztree multiple-cursors magit yasnippet symbol-overlay expand-region)))
+   '(modus-themes web-mode jq-mode highlight-function-calls dired-subtree just-mode justl difftastic diff-hl magit-todos powershell vterm doom-modeline flyspell-correct avy ace-window pdf-tools eglot edit-indirect nginx-mode polymode groovy-mode git-timemachine jinja2-mode flymake-collection yaml-pro vundo cape corfu wgrep vertico orderless embark-consult marginalia markdown-mode ztree multiple-cursors magit yasnippet symbol-overlay expand-region)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
